3a6c0b20b1a0efab78135d5296129ce15ec4a98c
Braughton <braughton.r@gmail.com>
2024-11-30 23:35:12 -0600

    Iniciando codigo y creando funciones basicas de movimiento

---
+++ Omnidirectional-self-driven-car.ino
@@ -0,0 +1,102 @@
+#define IN1 13
+#define IN2 12
+#define IN3 2
+#define IN4 3
+#define IN5 4
+#define IN6 5
+#define IN7 7
+#define IN8 6
+void setup(){
+  Serial.begin(115200);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(IN5, OUTPUT);
+  pinMode(IN6, OUTPUT);
+  pinMode(IN7, OUTPUT);
+  pinMode(IN8, OUTPUT);
+}
+void loop(){
+
+}
+void Frontal(int dir) {
+  int FD=map(50,0,255,PWMmin1,255)-map(dir,-78,78,0,200);
+  int FI=map(50,0,255,PWMmin2,255)+map(dir,-78,78,0,200);
+  int TD=map(50,0,255,PWMmin3,255)-map(dir,-78,78,0,200);
+  int TI=map(50,0,255,PWMmin4,255)+map(dir,-78,78,0,200);
+  digitalWrite(IN1, FD); 
+  digitalWrite(IN2, LOW); // Motor derecho frontal adelante
+  digitalWrite(IN3, FI); 
+  digitalWrite(IN4, LOW); // Motor izquierdo frontal adelante
+  digitalWrite(IN5, TD); 
+  digitalWrite(IN6, LOW); // Motor derecho trasero adelante
+  digitalWrite(IN7, TI); 
+  digitalWrite(IN8, LOW); // Motor izquierdo trasero adelante
+}
+void Reversa() {
+  int FD=map(20,0,255,PWMmin1,255);
+  int FI=map(20,0,255,PWMmin2,255);
+  int TD=map(20,0,255,PWMmin3,255);
+  int TI=map(20,0,255,PWMmin4,255);
+  digitalWrite(IN1, LOW); 
+  digitalWrite(IN2, FD); // Motor derecho frontal adelante
+  digitalWrite(IN3, LOW); 
+  digitalWrite(IN4, FI); // Motor izquierdo frontal adelante
+  digitalWrite(IN5, LOW); 
+  digitalWrite(IN6, TD); // Motor derecho trasero adelante
+  digitalWrite(IN7, LOW); 
+  digitalWrite(IN8, TI); // Motor izquierdo trasero adelante
+}
+void LateralD(){
+  int FD=map(20,0,255,PWMmin1,255);
+  int FI=map(20,0,255,PWMmin2,255);
+  int TD=map(20,0,255,PWMmin3,255);
+  int TI=map(20,0,255,PWMmin4,255);
+  digitalWrite(IN1, FD);  // GIRO Reversa
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);  // GIRO Frente
+  digitalWrite(IN4, FI);
+  digitalWrite(IN5, TD);  // GIRO Frente
+  digitalWrite(IN6, LOW);
+  digitalWrite(IN7, LOW);  // GIRO Revers
+  digitalWrite(IN8, TI);
+}
+void LateralI(){
+  int FD=map(20,0,255,PWMmin1,255);
+  int FI=map(20,0,255,PWMmin2,255);
+  int TD=map(20,0,255,PWMmin3,255);
+  int TI=map(20,0,255,PWMmin4,255);
+  digitalWrite(IN1, LOW);  // GIRO Reversa
+  digitalWrite(IN2, FD);
+  digitalWrite(IN3, FI);  // GIRO Frente
+  digitalWrite(IN4, LOW);
+  digitalWrite(IN5, LOW);  // GIRO Frente
+  digitalWrite(IN6, TD);
+  digitalWrite(IN7, TI);  // GIRO Revers
+  digitalWrite(IN8, LOW);
+}
+void Giro() {
+  int FD=map(200,0,255,PWMmin1,255);
+  int FI=map(200,0,255,PWMmin2,255);
+  int TD=map(200,0,255,PWMmin3,255);
+  int TI=map(200,0,255,PWMmin4,255);
+  digitalWrite(IN1, FD); 
+  digitalWrite(IN2, LOW); // Motor derecho frontal adelante
+  digitalWrite(IN3, LOW); 
+  digitalWrite(IN4, FI); // Motor izquierdo frontal atrás
+  digitalWrite(IN5, TD); 
+  digitalWrite(IN6, LOW); // Motor derecho trasero adelante
+  digitalWrite(IN7, LOW); 
+  digitalWrite(IN8, TI); // Motor izquierdo trasero atrás
+}
+void Alto() {
+  digitalWrite(IN1, LOW); 
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW); 
+  digitalWrite(IN4, LOW);
+  digitalWrite(IN5, LOW); 
+  digitalWrite(IN6, LOW);
+  digitalWrite(IN7, LOW); 
+  digitalWrite(IN8, LOW);
+}
